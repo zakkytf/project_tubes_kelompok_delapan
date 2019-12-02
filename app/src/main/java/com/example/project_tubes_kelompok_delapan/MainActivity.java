1c0dd771c7606ada6b47cc6a51e788721502e1c4
zakkytf <zakkytf@gmail.com>
2019-12-02 12:08:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/project_tubes_kelompok_delapan/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.project_tubes_kelompok_delapan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
