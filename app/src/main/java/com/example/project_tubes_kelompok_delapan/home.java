b47b6febdab68432f9c7ccb6e60e36567fe23645
zakkytf <zakkytf@gmail.com>
2019-12-02 16:46:47 +0700

    c

---
+++ app/src/main/java/com/example/project_tubes_kelompok_delapan/home.java
@@ -0,0 +1,14 @@
+package com.example.project_tubes_kelompok_delapan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
