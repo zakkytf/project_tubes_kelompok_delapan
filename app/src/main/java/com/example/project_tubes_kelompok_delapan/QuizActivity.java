2e4cfa5e4a913b65098cad0dcd584bc99e86fac5
zakkytf <zakkytf@gmail.com>
2019-12-05 14:14:41 +0700

    f

---
+++ app/src/main/java/com/example/project_tubes_kelompok_delapan/QuizActivity.java
@@ -0,0 +1,14 @@
+package com.example.project_tubes_kelompok_delapan;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
