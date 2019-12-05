4efc6594cd96db23298856a9417ffdaac21ae0f0
zakkytf <zakkytf@gmail.com>
2019-12-05 12:21:49 +0700

    d

--- app/src/main/java/com/example/project_tubes_kelompok_delapan/MainActivity.java
+++ app/src/main/java/com/example/project_tubes_kelompok_delapan/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.project_tubes_kelompok_delapan;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void tentang(View view) {
+        Intent intent = new Intent(MainActivity.this,about.class);
+        startActivity(intent);
+    }
+
+    public void mulai(View view) {
+        Intent intent = new Intent(MainActivity.this,start.class);
+        startActivity(intent);
     }
 }
