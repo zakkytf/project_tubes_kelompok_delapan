7064a42ee37f81c815d495a803d06788837e006b
zakkytf <zakkytf@gmail.com>
2019-12-05 14:54:38 +0700

    a1

--- app/src/main/java/com/example/project_tubes_kelompok_delapan/result.java
+++ app/src/main/java/com/example/project_tubes_kelompok_delapan/result.java
@@ -1,15 +1,14 @@
 package com.example.project_tubes_kelompok_delapan;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class QuizActivity extends AppCompatActivity {
+public class result extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        setContentView(R.layout.activity_result);
     }
 }
--- app/src/main/java/com/example/project_tubes_kelompok_delapan/start.java
+++ app/src/main/java/com/example/project_tubes_kelompok_delapan/start.java
@@ -2,7 +2,9 @@ package com.example.project_tubes_kelompok_delapan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class start extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    public void start_view(View view) {
+        Intent intent = new Intent(start.this,QuizActivity.class);
+        startActivity(intent);
     }
 }
